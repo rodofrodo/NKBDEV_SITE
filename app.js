9ce7814d64c7dc43e3e46c773c0c5f0a5a1561b2
rodofrodo <rodofrodo708@gmail.com>
2025-06-09 13:31:05 +0200

    Coded hamburger button

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-btn').onclick = function () {
+    document.getElementById('navbar').classList.toggle('active');
+    document.getElementById('hamburger-btn').classList.toggle('active');
+};
