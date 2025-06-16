f86b84505ca7882108a82d46025e94241277bc70
rodofrodo <rodofrodo708@gmail.com>
2025-06-16 20:48:12 +0200

    Menu hides when element is clicked

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-document.getElementById('hamburger-btn').onclick = function () {
+function toggleMenu() {
     document.getElementById('navbar').classList.toggle('active');
     document.getElementById('hamburger-btn').classList.toggle('active');
-};
+}
+
+document.getElementById('hamburger-btn').addEventListener('click', toggleMenu);
+document.getElementById('home-btn').addEventListener('click', toggleMenu);
+document.getElementById('products-btn').addEventListener('click', toggleMenu);
+document.getElementById('archives-btn').addEventListener('click', toggleMenu);
+document.getElementById('gallery-btn').addEventListener('click', toggleMenu);
+document.getElementById('about-btn').addEventListener('click', toggleMenu);
